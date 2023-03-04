29ec3e6470dc01eb56835fb417f9334be682ae43
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2023-03-04 16:11:22 -0300

    adjusting profile file

--- src/main/java/br/com/vsc/raffle/model/Profile.java
+++ src/main/java/br/com/vsc/raffle/model/Profile.java
@@ -18,18 +18,17 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity
 public class Profile implements GrantedAuthority {
-	
-	private static final long serialVersionUID = 1L;
 
-	@Id
+    private static final long serialVersionUID = 1L;
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-	
+
     private String name;
 
-	@Override
-	public String getAuthority() {
-		return this.name;
-	}
-
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }
 }
